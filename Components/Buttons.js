70189deb1aab0c35c7ec39c138df2983bdfd668f
Pratham Meghani <premmeghani16@gmail.com>
2021-06-20 13:24:40 +0530

    ButtonsAdded

--- Components/Buttons.js
+++ Components/Buttons.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Button } from "native-base";
+import { Button ,Icon} from "native-base";
 const Buttons = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
-      <Button full>
+      <Button full success style={{margin:20}}>
         <Text>Click here</Text>
       </Button>
+      <Button full warning bordered style={{margin:20}}>
+        <Text>Click here</Text>
+      </Button>
+      <Button full  transparent style={{margin:20}}>
+        <Text>Click here</Text>
+      </Button>
+      <Button full danger block style={{margin:20}}>
+        <Text>Click here</Text>
+      </Button>
+      <Button full info rounded style={{margin:20}}>
+        <Text>Click here</Text>
+      </Button>
+      <Button iconRight light full style={{margin:20}}>
+            <Text>Next</Text>
+            <Icon name='home' />
+          </Button>
     </View>
   );
 };
