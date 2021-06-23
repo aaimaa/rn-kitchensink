e0590c33b31c2f9dd2be32f0025a7906d660591a
Pratham Meghani <premmeghani16@gmail.com>
2021-06-23 09:41:04 +0530

    ColumnListItemImplemented

--- Components/ListItems.js
+++ Components/ListItems.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View,Image } from 'react-native'
-import { Container,Content } from 'native-base'
+import { Container,Content,Body,Card,CardItem } from 'native-base'
 const ListItems = () => {
     return (
        <Container>
@@ -16,7 +16,24 @@ const ListItems = () => {
         />
          <Text style={{ color: "black", fontSize: 17,flex:1,paddingTop:25,marginLeft:12 }}>Flutter Vs React Native Lets Compare</Text>
          </View>
+        
+     
            </Content>
+           <View style={{ margin: 8, borderRadius: 12, flex: 1 }}>
+           <Card style={{ borderRadius: 12 }}>
+          <CardItem cardBody>
+            <Image
+              source={{ uri: 'https://www.qed42.com/sites/default/files/2018-11/react-native.png' }}
+              style={{ height: 200, width: null, flex: 1 }}
+            />
+          </CardItem>
+          <Body>
+            <Text style={{ marginLeft: 5, padding: 5, fontWeight: "700" }}>
+              React Native:Basics and Introduction
+            </Text>
+          </Body>
+        </Card>
+      </View>
        </Container>
     )
 }
